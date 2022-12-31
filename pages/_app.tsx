245cb42978027e6d474a8cdd3f704cae2228fd52
Jang Haemin <io@jhaemin.com>
2023-01-01 01:05:09 +0900

    Add open graph information

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -17,9 +17,18 @@ const App = ({ Component, pageProps }: AppProps) => {
     <>
       <Head>
         <title>라이어 게임</title>
+        <meta property="og:title" content="라이어 게임" />
         <meta name="description" content="우리 중 누군가 거짓말을 하고 있다." />
+        <meta
+          property="og:description"
+          content="우리 중 누군가 거짓말을 하고 있다."
+        />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
+        <meta
+          property="og:image"
+          content="https://www.liar-game.com/og/open_graph.png"
+        />
       </Head>
       <div id="app" className={styles.wrapper}>
         <Component {...pageProps} />

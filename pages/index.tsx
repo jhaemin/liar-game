57029ca0b3a6e80b50b19d144a700c7a98092df6
Jang Haemin <io@jhaemin.com>
2023-01-01 11:30:57 +0900

    Add open source link and polish layout

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,16 +1,36 @@
+import { LogoGithub } from 'framework7-icons-plus/react'
 import Link from 'next/link'
 import styles from './home.module.scss'
 
 const Home = () => {
   return (
     <div className={styles.home}>
-      <div className={styles.hero}>
-        <h2 className={styles.titleEng}>Liar Game</h2>
-        <h1 className={styles.title}>라이어 게임</h1>
+      <div className={styles.top} />
+
+      <div className={styles.middle}>
+        <div className={styles.hero}>
+          <h2 className={styles.titleEng}>Liar Game</h2>
+          <h1 className={styles.title}>라이어 게임</h1>
+        </div>
+        <Link href="/room/create">
+          <button>방 만들기</button>
+        </Link>
+        <Link href="/room/join">
+          <button className="minimal">플레이 방법</button>
+        </Link>
       </div>
-      <Link href="/room/create">
-        <button>방 만들기</button>
-      </Link>
+
+      <div className={styles.bottom}>
+        <a
+          href="https://github.com/jhaemin/liar-game"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <span className={styles.source}>
+            <LogoGithub className={styles.icon} /> GitHub
+          </span>
+        </a>
+      </div>
     </div>
   )
 }
